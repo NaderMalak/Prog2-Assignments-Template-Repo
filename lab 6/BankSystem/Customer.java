8cec58c18923545c2fa47a986997ea9330152d54
NaderMalak <128903948+NaderMalak@users.noreply.github.com>
2023-05-06 16:41:45 +0300

    Add files via upload

---
+++ lab 6/BankSystem/Customer.java
@@ -0,0 +1,16 @@
+// Nader Malak
+// 20206467
+package BankSystem;
+
+public class Customer extends Human {
+    private String SSN;
+    private String address;
+    private BankAccount bankAcc;
+
+    public Customer(String n, int a, char g, String s, String ad) {
+        super(n, a, g);
+        this.SSN = s;
+        this.address = ad;
+        this.bankAcc = new BankAccount();
+    }
+}
